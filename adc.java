aed395d45526e20f79c62eccacd7a168242d827b
Punya2208 <121537040+Punya2208@users.noreply.github.com>
2023-02-10 05:01:33 -0800

    Update adc.java

--- adc.java
+++ adc.java
@@ -2,16 +2,10 @@ class Abc
 {
 static void add ()
 {
-int a = 10;
-int b = 20;
+int a = 20;
+int b = 60;
 int c = a+b;
 }
-  {
-    static void add ()
-      int a = 10;
-      int b = 20;
-       int c = a+b;
-   }
   public static void main (String[] args)
   {
     add();
